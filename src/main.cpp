5f461f7ee8d6ac05b5872690b70bd1a3e0306d29
Deepcong2019 <735005214@qq.com>
2024-10-19 18:44:43 +0800

    rename video test yolo  to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-
-#include <builder/trt_builder.hpp>
-#include <infer/trt_infer.hpp>
-#include <common/ilogger.hpp>
 #include "app_yolo/yolo.hpp"
-#include "app_yolo/multi_gpu.hpp"
+#include "common/ilogger.hpp"
+#include "builder/trt_builder.hpp"
+#include "infer/trt_infer.hpp"
 
 using namespace std;
+bool requires(const char* name);
 
 static const char* cocolabels[] = {
     "person", "bicycle", "car", "motorcycle", "airplane",
@@ -23,23 +22,39 @@ static const char* cocolabels[] = {
     "scissors", "teddy bear", "hair drier", "toothbrush"
 };
 
-bool requires(const char* name);
+int main() {
+    TRT::Mode mode = TRT::Mode::FP32;
+    Yolo::Type type = Yolo::Type::V7;
+    string model = "yolov7";
+    int deviceid = 0;
+    auto mode_name = TRT::mode_string(mode);
+    TRT::set_device(deviceid);
 
-static void append_to_file(const string& file, const string& data) {
-    FILE* f = fopen(file.c_str(), "a+");
-    if (f == nullptr) {
-        INFOE("Open %s failed.", file.c_str());
-        return;
+
+
+    const char* name = model.c_str();
+    INFO("===================== test %s %s %s ==================================", Yolo::type_name(type), mode_name, name);
+
+    if (not requires(name))
+        return 0;
+
+    string onnx_file = iLogger::format("%s.onnx", name);
+    string model_file = iLogger::format("%s.%s.trtmodel", name, mode_name);
+    int test_batch_size = 16;
+
+
+    // ±àÒëÄ£ÐÍ
+    if (not iLogger::exists(model_file)) {
+        TRT::compile(
+            mode,                       // FP32¡¢FP16¡¢INT8
+            test_batch_size,            // max batch size
+            onnx_file,                  // source 
+            model_file                 // save to
+         );
     }
 
-    fprintf(f, "%s\n", data.c_str());
-    fclose(f);
-}
-
-static void inference_and_performance(int deviceid, const string& engine_file, TRT::Mode mode, Yolo::Type type, const string& model_name) {
-
     auto engine = Yolo::create_infer(
-        engine_file,                // engine file
+        model_file,                // engine file
         type,                       // yolo type, Yolo::Type::V5 / Yolo::Type::X
         deviceid,                   // gpu id
         0.25f,                      // confidence threshold
@@ -50,113 +65,65 @@ static void inference_and_performance(int deviceid, const string& engine_file, T
     );
     if (engine == nullptr) {
         INFOE("Engine is nullptr");
-        return;
+        return 0;
     }
 
-    auto files = iLogger::find_files("inference", "*.jpg;*.jpeg;*.png;*.gif;*.tif");
-    vector<cv::Mat> images;
-    for (int i = 0; i < files.size(); ++i) {
-        auto image = cv::imread(files[i]);
-        images.emplace_back(image);
+    string video_path = "./exp/face_tracker.mp4";
+    Yolo::BoxArray boxes_array;
+
+    // ´´½¨ VideoCapture ¶ÔÏó
+    cv::VideoCapture cap(video_path);
+
+    // ¼ì²éÊÓÆµÊÇ·ñ³É¹¦´ò¿ª
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Could not open video file." << std::endl;
+        return -1;
     }
 
-    // warmup
-    vector<shared_future<Yolo::BoxArray>> boxes_array;
-    for (int i = 0; i < 10; ++i)
-        boxes_array = engine->commits(images);
-    boxes_array.back().get();
-    boxes_array.clear();
+    // ¶ÁÈ¡²¢ÏÔÊ¾ÊÓÆµÖ¡
+    cv::Mat frame;
+    while (true) {
+        auto begin_timer = iLogger::timestamp_now_float();
+        cap >> frame; // ¶ÁÈ¡Ò»Ö¡
+        if (frame.empty()) {
+            break; // ÊÓÆµ½áÊø
+        }
+        boxes_array = engine->commit(frame).get();
+        float fps = 1000 / (iLogger::timestamp_now_float() - begin_timer);
+       // ½« fps ×ª»»Îª×Ö·û´®£¨Ê¹ÓÃÈÎÒ»·½·¨£©
+        std::string fps_str = std::to_string(fps);  // »òÕßÊ¹ÓÃ×Ö·û´®Á÷·½·¨
 
-    /////////////////////////////////////////////////////////
-    const int ntest = 100;
-    auto begin_timer = iLogger::timestamp_now_float();
+        // ÔÚÊÓÆµÖ¡ÉÏÏÔÊ¾ FPS
+        cv::putText(frame, "FPS: " + fps_str, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
 
-    for (int i = 0; i < ntest; ++i)
-        boxes_array = engine->commits(images);
-
-    // wait all result
-    boxes_array.back().get();
-
-    float inference_average_time = (iLogger::timestamp_now_float() - begin_timer) / ntest / images.size();
-    auto type_name = Yolo::type_name(type);
-    auto mode_name = TRT::mode_string(mode);
-    INFO("%s[%s] average: %.2f ms / image, FPS: %.2f", engine_file.c_str(), type_name, inference_average_time, 1000 / inference_average_time);
-    append_to_file("perf.result.log", iLogger::format("%s,%s,%s,%f", model_name.c_str(), type_name, mode_name, inference_average_time));
-
-    string root = iLogger::format("%s_%s_%s_result", model_name.c_str(), type_name, mode_name);
-    iLogger::rmtree(root);
-    iLogger::mkdir(root);
-
-    for (int i = 0; i < boxes_array.size(); ++i) {
-
-        auto& image = images[i];
-        auto boxes = boxes_array[i].get();
-
-        for (auto& obj : boxes) {
+        for (auto& obj : boxes_array) {
             uint8_t b, g, r;
             tie(b, g, r) = iLogger::random_color(obj.class_label);
-            cv::rectangle(image, cv::Point(obj.left, obj.top), cv::Point(obj.right, obj.bottom), cv::Scalar(b, g, r), 5);
+            cv::rectangle(frame, cv::Point(obj.left, obj.top), cv::Point(obj.right, obj.bottom), cv::Scalar(b, g, r), 5);
 
             auto name = cocolabels[obj.class_label];
-            auto caption = iLogger::format("%s %.2f", name, obj.confidence);
+            auto caption = iLogger::format("%s %.2f %.2f", name, obj.confidence);
             int width = cv::getTextSize(caption, 0, 1, 2, nullptr).width + 10;
-            cv::rectangle(image, cv::Point(obj.left - 3, obj.top - 33), cv::Point(obj.left + width, obj.top), cv::Scalar(b, g, r), -1);
-            cv::putText(image, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 2, 16);
+            cv::rectangle(frame, cv::Point(obj.left - 3, obj.top - 33), cv::Point(obj.left + width, obj.top), cv::Scalar(b, g, r), -1);
+
+            cv::putText(frame, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 2, 16);
+            cv::imshow("Video", frame);
+            
         }
 
-        string file_name = iLogger::file_name(files[i], false);
-        string save_path = iLogger::format("%s/%s.jpg", root.c_str(), file_name.c_str());
-        INFO("Save to %s, %d object, average time %.2f ms", save_path.c_str(), boxes.size(), inference_average_time);
-        cv::imwrite(save_path, image);
+      
+        if (cv::waitKey(30) == 27) { // °´ÏÂ ESC ¼üÍË³ö
+            break;
+        }
     }
     engine.reset();
-}
 
-static void test(Yolo::Type type, TRT::Mode mode, const string& model) {
+    // ÊÍ·Å×ÊÔ´
+    cap.release();
+    cv::destroyAllWindows();
 
-    int deviceid = 0;
-    auto mode_name = TRT::mode_string(mode);
-    TRT::set_device(deviceid);
 
-    auto int8process = [=](int current, int count, const vector<string>& files, shared_ptr<TRT::Tensor>& tensor) {
-
-        INFO("Int8 %d / %d", current, count);
-        for (int i = 0; i < files.size(); ++i) {
-            auto image = cv::imread(files[i]);
-            Yolo::image_to_tensor(image, tensor, type, i);
-        }
-        };
-
-    const char* name = model.c_str();
-    INFO("===================== test %s %s %s ==================================", Yolo::type_name(type), mode_name, name);
-
-    if (not requires(name))
-        return;
-
-    string onnx_file = iLogger::format("%s.onnx", name);
-    string model_file = iLogger::format("%s.%s.trtmodel", name, mode_name);
-    int test_batch_size = 16;
-
-    if (not iLogger::exists(model_file)) {
-        TRT::compile(
-            mode,                       // FP32ã€FP16ã€INT8
-            test_batch_size,            // max batch size
-            onnx_file,                  // source 
-            model_file,                 // save to
-            {},                         // é‡å®šä¹‰è¾“å…¥çš„shape
-            int8process,                // æŒ‡å®šint8æ ‡å®šæ•°æ®çš„å¤„ç†å›žè°ƒå‡½æ•°
-            "inference"                 // int8æ ‡å®šæ•°æ®æ‰€åœ¨ç›®å½•
-        );
-    }
-    
-    inference_and_performance(deviceid, model_file, mode, type, name);
+    return 0;
 }
 
 
-
-//int main() {
-//
-//    test(Yolo::Type::V7, TRT::Mode::FP32, "yolov7");
-//
-//    return 0;
-//}
